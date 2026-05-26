a05f9f4af87c431a45af72ffcb4661770f9308c5
not-louis-239 <louis.boulton@icloud.com>
2026-05-26 22:38:46 +0800

    Use shorter normalised do-until loop start variable in exam.py and add do-until tests

---
+++ tests/fixtures/marking/ok/q005_until_ops.py
@@ -0,0 +1,6 @@
+x = 0
+__start_1234__ = [True]
+while (__start_1234__ and __start_1234__.pop()) or not (x >= 10):
+    x += 1
+    print(f"right now, x is {x}")
+print("x is now greater than or equal to 10")
