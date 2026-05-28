71d524fe726123e0c285d3be4f65f7987941d256
not-louis-239 <louis.boulton@icloud.com>
2026-05-28 11:48:49 +0800

    Add logic in interpreter to print tracebacks in response to exceptions

--- boot/cputh/mains/sing.py
+++ boot/cputh/mains/sing.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from cputh.utils.args import Args
 from cputh.exceptions.errors import CPuthFileError
 from cputh.compile.compiler import compile_cputh_to_py
+from cputh.utils.format_exceptions import format_exc
 
 def validate_args(args: Args) -> None:
     if not args.input is not None:
@@ -17,15 +18,25 @@ def main(args: Args) -> int:
 
     assert args.input is not None
 
-    cputh_code = Path(args.input).read_text()
-    py_code = compile_cputh_to_py(cputh_code)
+    cputh_code = args.input.read_text()
 
     try:
-        exec(py_code, {})
+        py_code = compile_cputh_to_py(cputh_code)
+    except Exception as exc:
+        fmted_exc = format_exc(exc, is_runtime_err=False)
+        print(fmted_exc)
+        return 1
+
+    try:
+        exec(py_code, {"__name__": "__main__"})
         return 0
-    except KeyboardInterrupt:
+    except KeyboardInterrupt as exc:
+        fmted_exc = format_exc(exc, is_runtime_err=True)
+        print(fmted_exc)
         return 130
-    except BaseException:
+    except BaseException as exc:
+        fmted_exc = format_exc(exc, is_runtime_err=True)
+        print(fmted_exc)
         return 1
 
 if __name__ == "__main__":
